1f1fb884dceba93674488db485701d2c6f405a55
hariomGiri <sureshgiri469@gmail.com>
2024-11-22 20:31:03 +0530

    chages in index

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/project-bolt-sb1/',
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
 });
