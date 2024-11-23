f2e86219d4be7b25a8e3f2c3f8dce9b5d3010565
hariomGiri <sureshgiri469@gmail.com>
2024-11-23 13:11:12 +0530

    handle white page error

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/project-bolt-sb1/',
-  assetsInclude: ['**/*.jpg', '**/*.png'],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 });
